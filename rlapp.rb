370cc8a2b518e2a87d077005c4862b2282d84806
Satish Talim <satish.talim@gmail.com>
2010-11-16 17:14:24 +0530

    rlapp first commit

--- rlapp.rb
+++ rlapp.rb
@@ -2,8 +2,8 @@
 require 'sinatra'
 
 # Here you have to put your own Application ID and Secret
-APP_ID = '155928607784317'
-APP_SECRET = 'bbc85a85e967a751d451ac20dbcea506'
+APP_ID = '102013213204014'
+APP_SECRET = '271969c9ac8e46949a5eb2046b0592cd'
 
 get '/' do
   erb :index
